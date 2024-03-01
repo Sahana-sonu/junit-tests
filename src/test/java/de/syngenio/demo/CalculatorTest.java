bf0392f86a45b083c81e54cc03c0744059e80b97
Sahana-sonu <93938850+Sahana-sonu@users.noreply.github.com>
2024-03-01 12:24:07 +0530

    Update CalculatorTest.java

--- src/test/java/de/syngenio/demo/CalculatorTest.java
+++ src/test/java/de/syngenio/demo/CalculatorTest.java
@@ -1,19 +1,24 @@
 package de.syngenio.demo;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
 public class CalculatorTest {
 
-    
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        int result = calculator.add(2, 3);
+        if (result == 5) {
+            System.out.println("Addition Test Passed");
+        } else {
+            System.out.println("Addition Test Failed");
+        }
     }
 
-   
     public void testSubtract() {
         Calculator calculator = new Calculator();
-        assertEquals(1, calculator.subtract(4, 3));
+        int result = calculator.subtract(4, 3);
+        if (result == 1) {
+            System.out.println("Subtraction Test Passed");
+        } else {
+            System.out.println("Subtraction Test Failed");
+        }
     }
 }
